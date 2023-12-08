1999dcf014eb7b252aa8127f76c22b01bf2e9e69
Tahir Gani <tahir.gani@flexcar.com>
2023-12-08 20:26:25 +0530

    Basic todo app setup

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,82 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: %i[ show edit update destroy ]
+  before_action :find_all_todos, except: [:destroy]
+
+  # GET /todos or /todos.json
+  def index
+    @todos = Todo.all
+  end
+
+  # GET /todos/1 or /todos/1.json
+  def show
+  end
+
+  # GET /todos/new
+  def new
+    @todo = Todo.new
+  end
+
+  # GET /todos/1/edit
+  def edit
+    @todo = Todo.find(params.require(:id))
+  end
+
+  # POST /todos or /todos.json
+  def create
+    @todo = Todo.new(todo_params)
+
+    respond_to do |format|
+      if @todo.save
+        format.html { redirect_to new_todo_path, notice: "Todo '" + @todo.title + "' was successfully created." }
+        format.json { render :show, status: :created, location: @todo }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @todo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /todos/1 or /todos/1.json
+  def update
+    respond_to do |format|
+      if @todo.update(todo_params)
+        format.html { redirect_to todo_url(@todo), notice: "Todo '" + @todo.title + "' was successfully updated." }
+        format.json { render :show, status: :ok, location: @todo }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @todo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /todos/1 or /todos/1.json
+  def destroy
+    @todo.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to new_todo_path, notice: "Todo '" + @todo.title + "' was successfully deleted." }
+      format.json { head :no_content }
+    end
+  end
+
+  def toggle_todo
+    @todo = Todo.find(params.require(:id))
+    @todo.update(completed: !@todo.completed)
+    redirect_to new_todo_path
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def todo_params
+      params.require(:todo).permit(:title, :completed)
+    end
+
+    def find_all_todos
+      @todos = Todo.all
+    end
+end
